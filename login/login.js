c3ab8dcd1c378193b28179df68be9532d0ac3ce3
EnioAmarantes <enio.amarantes@gmail.com>
2021-06-25 08:03:44 -0300

    implementação dos artigos e login

--- login/login.js
+++ login/login.js
@@ -15,3 +15,20 @@ function login(){
     else
         alert("Não funcionou o login");
 }
+
+function artigo(id){
+
+    switch(id){
+        case 1:
+            window.open('https://www.rbmt.org.br/details/1583/pt-BR/covid-19--a-importancia-da-fisioterapia-na-recuperacao-da-saude-do-trabalhador');
+            break;
+        case 2:
+            window.open('https://wwww.google.com.br');
+            break;
+        case 3:
+            window.open('https://www.google.com.br');
+            break;
+        default:
+            alert("link para o artigo não encontrado.");
+    }
+}
